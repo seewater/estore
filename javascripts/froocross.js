ff196858caae98dd66c128806f98fae9c78c568a
seewater <ap99031@hotmail.com>
2015-06-18 22:11:17 -0400

    Update froocross.js

--- javascripts/froocross.js
+++ javascripts/froocross.js
@@ -9,7 +9,7 @@ function buildFrooGallery() {
 	document.getElementById('froo-gallery-wrap').style.background = 'url('+newImage+') no-repeat';
 	document.getElementById('froo-gallery-wrap').style.height = "340px";
 	document.getElementById('froo-gallery-wrap').style.width = "600px";
-	document.getElementById('froo-gallery-wrap').style.marginTop = "20px";
+	document.getElementById('froo-gallery-wrap').style.marginTop = "10px";
 	var ScrollImages=new Array(); 
 	for (var i=0; i<=ListingNum.length; i++){
 		if (ListingNum[i]){
@@ -47,7 +47,7 @@ function buildFrooGallery() {
 	xHTML+='<td id="froo-gallery-price" align="right" width="115px"></td>';
 	xHTML+='</table></td></tr></table>';
 	xHTML+='<div style="clear:both;"></div>';
-	xHTML+='<div style="clear:both;height:10px;">&nbsp;</div></div>';
+	xHTML+='<div style="clear:both;height:60px;">&nbsp;</div></div>';
 	
 	document.getElementById('froo-gallery-wrap').innerHTML=xHTML;
 	scroller1=document.getElementById("froo-slide1");
